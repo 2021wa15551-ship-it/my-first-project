a78696ec0dcebb4ad0b0957af1192c9c23a17b9c
Pruthvi <2021wa15551.bits-pilani.ac.in>
2026-04-27 11:13:20 +0000

    added to all

--- index.js
+++ index.js
@@ -5,4 +5,6 @@ app.get("/", (req, res) => {
   res.send("CI/CD working 🚀");
 });
 
-app.listen(3000, () => console.log("Running"));
+app.listen(3000, "0.0.0.0", () => {
+  console.log("Running");
+});
